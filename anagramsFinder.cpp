c071d04c3f3b0bf9613c440cd0922bbae30dcb74
Adrian Goldwaser <agold@hotmail.com>
2014-02-14 21:36:03 +1100

    fixed indenting

--- anagramsFinder.cpp
+++ anagramsFinder.cpp
@@ -1,9 +1,9 @@
 /***********************************************************************
  ********************** Siteswap Anagram Finder ************************
  ************************** Adrian Goldwaser ***************************
- * Usage:															   *
- *   via command line:  ./anagrams <siteswap>						   *
- *   or simply run the program and enter siteswap on prompt			   *
+ * Usage:                                                              *
+ *   via command line:  ./anagrams <siteswap>                          *
+ *   or simply run the program and enter siteswap on prompt            *
  *   Put a '?' at the front of the siteswap to only output the count   *
  ***********************************************************************/
 
